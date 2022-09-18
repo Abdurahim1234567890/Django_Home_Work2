374b6c4323375c4c9bc0a564a6dae1d86e3204c3
Abdurahim1234567890 <dzhanbakkiev@mail.ru>
2022-09-18 18:18:54 +0600

    My Django project

--- posts/models.py
+++ posts/models.py
@@ -17,3 +17,11 @@ class Post(models.Model):
     date = models.DateField(auto_now_add=True, blank=True, null=True)
     stars = models.IntegerField(null=True)
     type = models.IntegerField(choices=POST_TYPE_CHOICES, null=True)
+
+
+
+class Comment(models.Model):
+    Nikname = models.CharField(max_length=20)
+    data = models.DateField(auto_now_add=True, blank=True, null=False)
+    comment = models.TextField(max_length=69)
+    stars = models.IntegerField(max_length=5, blank=False)
